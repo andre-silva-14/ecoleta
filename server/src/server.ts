2eb4f7983a69114a03ea6ed7fe93ff9374e700b1
andre-silva-14 <silva.andre1997@gmail.com>
2020-06-05 18:06:27 +0200

    Add Form Validation to createPoint routes
    Form validation has been added to the route calling
    PointsController.Create using celebrate (form validation framework).

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import path from 'path';
+import { errors } from 'celebrate';
 
 
 const app = express();
@@ -9,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
+app.use(errors());
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
