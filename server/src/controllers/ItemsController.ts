02ce376ad9f94869139ecd1aedeb4fd2e0434888
andre-silva-14 <silva.andre1997@gmail.com>
2020-06-07 18:10:39 +0200

    API now retrieves a dynamic host for listing Items
    - API will now list items with the host URL to which it was called upon.
    - This feature enables the server to run on different hosts and
    dinamically alocate the item URL depending on each platform's
    dependencies.

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -4,12 +4,13 @@ import { Request, Response } from 'express';
 class ItemsController {
     async index(request: Request, response: Response) {
         const items = await knex('items').select('*');
+        const host = request.headers.host;
     
         const serializedItems = items.map(item => {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`
+                image_url: `https://${host}/uploads/${item.image}`
             };
         });
     
